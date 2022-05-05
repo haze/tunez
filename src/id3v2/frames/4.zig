e936f1f81382c90c0b913fb4f3ca86e1b90a3c34
Haze Booth <isnt@haze.cool>
2022-05-05 12:05:05 -0400

    add proper extended header support for id3v2.4
    Former-commit-id: a7dcda0130e2f5a93f1aada9d6de395e79322fca

--- src/id3v2/frames/4.zig
+++ src/id3v2/frames/4.zig
@@ -53,11 +53,218 @@ pub const Frame = union(enum) {
     TXXX: frames.TXXX,
 };
 
+const ExtendedHeaderFlagKind = enum {
+    crc_data_present,
+    tag_is_update,
+    tag_has_restrictions,
+};
+
+pub const ExtendedHeaderTagRestrictions = struct {
+    pub const FrameSizeRestriction = enum {
+        no_more_than_128_frames_1mb_tag_size,
+        no_more_than_64_frames_128kb_tag_size,
+        no_more_than_32_frames_40kb_tag_size,
+        no_more_than_32_frames_4kb_tag_size,
+    };
+
+    pub const TextEncodingRestriction = enum {
+        only_iso_8859_1_or_utf8,
+    };
+
+    pub const TextFieldSizeRestriction = enum {
+        no_string_longer_than_1024_char,
+        no_string_longer_than_128_char,
+        no_string_longer_than_30_char,
+    };
+
+    pub const ImageEncodingRestriction = enum { png_or_jpeg_only };
+
+    pub const ImageSizeRestriction = enum {
+        all_are_256x256_or_smaller,
+        all_are_64x64_or_smaller,
+        all_are_exactly_64x64,
+    };
+
+    frame_size_restriction: FrameSizeRestriction,
+    text_encoding_restriction: ?TextEncodingRestriction,
+    text_field_size_restriction: ?TextFieldSizeRestriction,
+    image_encoding_restriction: ?ImageEncodingRestriction,
+    image_size_restriction: ?ImageSizeRestriction,
+
+    fn fromByte(source: u8) ExtendedHeaderTagRestrictions {
+        var restrictions: ExtendedHeaderTagRestrictions = undefined;
+
+        // 0xPP000000
+        restrictions.frame_size_restriction = switch (source & 0b11000000) {
+            0b11000000 => .no_more_than_128_frames_1mb_tag_size,
+            0b10000000 => .no_more_than_64_frames_128kb_tag_size,
+            0b01000000 => .no_more_than_32_frames_40kb_tag_size,
+            0b00000000 => .no_more_than_32_frames_4kb_tag_size,
+            else => unreachable,
+        };
+
+        restrictions.text_encoding_restriction = switch (source & 0b00100000) {
+            0b00100000 => .only_iso_8859_1_or_utf8,
+            0b00000000 => null,
+            else => unreachable,
+        };
+
+        restrictions.text_field_size_restriction = switch (source & 0b00011000) {
+            0b00011000 => .no_string_longer_than_30_char,
+            0b00001000 => .no_string_longer_than_1024_char,
+            0b00010000 => .no_string_longer_than_128_char,
+            0b00000000 => null,
+            else => unreachable,
+        };
+
+        restrictions.image_encoding_restriction = switch (source & 0b00000100) {
+            0b00000100 => .png_or_jpeg_only,
+            0b00000000 => null,
+            else => unreachable,
+        };
+
+        restrictions.image_size_restriction = switch (source & 0b00000011) {
+            0b00000011 => .all_are_exactly_64x64,
+            0b00000010 => .all_are_64x64_or_smaller,
+            0b00000001 => .all_are_256x256_or_smaller,
+            0b00000000 => null,
+            else => unreachable,
+        };
+
+        return restrictions;
+    }
+};
+
+// TODO(haze): look into turning the CRC into a [5]u8
+pub const ExtendedHeader = struct {
+    allocator: std.mem.Allocator,
+    flags: []ExtendedHeaderFlag,
+
+    pub fn format(
+        self: ExtendedHeader,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+        try writer.writeAll("ExtendedHeader { flags: [");
+        for (self.flags) |flag|
+            try writer.print("{}", .{flag});
+        try writer.writeAll("]}");
+    }
+
+    pub fn deinit(self: *ExtendedHeader) void {
+        for (self.flags) |*flag|
+            flag.deinit(self.allocator); // free the crc data
+        self.allocator.free(self.flags);
+        self.* = undefined;
+    }
+};
+
+pub const ExtendedHeaderFlag = struct {
+    /// If this flag is set, the present tag is an update of a tag found
+    /// earlier in the present file or stream. If frames defined as unique
+    /// are found in the present tag, they are to override any
+    /// corresponding ones found in the earlier tag. This flag has no
+    /// corresponding data.
+    tag_is_update: bool,
+
+    // If this flag is set, a CRC-32 [ISO-3309] data is included in the
+    // extended header. The CRC is calculated on all the data between the
+    // header and footer as indicated by the header's tag length field,
+    // minus the extended header. Note that this includes the padding (if
+    // there is any), but excludes the footer. The CRC-32 is stored as an
+    // 35 bit synchsafe integer, leaving the upper four bits always
+    // zeroed.
+    maybe_crc_data: ?[]u8,
+
+    // For some applications it might be desired to restrict a tag in more
+    // ways than imposed by the ID3v2 specification. Note that the
+    // presence of these restrictions does not affect how the tag is
+    // decoded, merely how it was restricted before encoding. If this flag
+    // is set the tag is restricted as follows:
+    maybe_restrictions: ?ExtendedHeaderTagRestrictions,
+
+    pub fn format(
+        self: ExtendedHeaderFlag,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = options;
+        _ = fmt;
+
+        try writer.print("ExtHeaderFlag {{ tag_is_update: {}, ", .{self.tag_is_update});
+
+        if (self.maybe_crc_data) |crc_data| {
+            try writer.print("crc_data: <{} bytes>, ", .{crc_data.len});
+        } else {
+            try writer.writeAll("crc_data: null, ");
+        }
+
+        if (self.maybe_restrictions) |restrictions| {
+            try writer.print("restrictions: {}", .{restrictions});
+        } else {
+            try writer.writeAll("restrictions: null");
+        }
+
+        try writer.writeAll(" }");
+    }
+
+    pub fn deinit(self: *ExtendedHeaderFlag, allocator: std.mem.Allocator) void {
+        if (self.maybe_crc_data) |crc_data|
+            allocator.free(crc_data);
+        self.* = undefined;
+    }
+};
+
 pub const RawExtendedHeader = struct {
+    pub const TagIsUpdateMask = 1 << 6;
+    pub const HasCrcDataMask = 1 << 5;
+    pub const HasTagRestrictionMask = 1 << 4;
+
     allocator: std.mem.Allocator,
 
     flags: []u8,
     size: u32,
+
+    pub fn interpret(self: RawExtendedHeader, reader: anytype) !ExtendedHeader {
+        var extended_header: ExtendedHeader = undefined;
+
+        extended_header.allocator = self.allocator;
+        extended_header.flags = try self.allocator.alloc(ExtendedHeaderFlag, self.flags.len);
+
+        for (self.flags) |flag, index| {
+            log.warn("flag[{}] = {b}", .{ index, flag });
+            var extended_header_flag: ExtendedHeaderFlag = undefined;
+
+            extended_header_flag.tag_is_update = flag & TagIsUpdateMask != 0;
+
+            if (flag & HasCrcDataMask != 0) {
+                const crc_data_length = try reader.readByte();
+                log.warn("crc_len={}", .{crc_data_length});
+                var crc_data = try self.allocator.alloc(u8, crc_data_length);
+                _ = try reader.readAll(crc_data);
+                extended_header_flag.maybe_crc_data = crc_data;
+            } else {
+                extended_header_flag.maybe_crc_data = null;
+            }
+
+            if (flag & HasTagRestrictionMask != 0) {
+                const tag_restriction_size = try reader.readByte();
+                _ = tag_restriction_size;
+                const raw_restrictions = try reader.readByte();
+                extended_header_flag.maybe_restrictions = ExtendedHeaderTagRestrictions.fromByte(raw_restrictions);
+            } else {
+                extended_header_flag.maybe_restrictions = null;
+            }
+
+            extended_header.flags[index] = extended_header_flag;
+        }
+
+        return extended_header;
+    }
 
     pub fn deinit(self: *RawExtendedHeader) void {
         self.allocator.free(self.flags);
@@ -108,7 +315,7 @@ pub fn Parser(comptime ReaderType: type) type {
 
         pub const Result = union(enum) {
             header: Header,
-            extended_header: RawExtendedHeader,
+            extended_header: ExtendedHeader,
             frame: Frame,
             unknown_frame: struct {
                 frame_id: []const u8,
@@ -125,7 +332,7 @@ pub fn Parser(comptime ReaderType: type) type {
                 _ = options;
                 return switch (self) {
                     .header => |header| writer.print("Header {}", .{header}),
-                    .extended_header => |header| writer.print("Extended Header {}", .{header}),
+                    .extended_header => |header| writer.print("{}", .{header}),
                     .frame => |frame| writer.print("Frame {}", .{frame}),
                     .unknown_frame => |data| writer.print("Unkowwn Frame {s}", .{data.frame_id}),
                 };
@@ -142,7 +349,8 @@ pub fn Parser(comptime ReaderType: type) type {
                         .TRCK, .TPOS, .TBPM, .TDLY, .TLEN => {},
                     },
                     .unknown_frame => |data| data.allocator.free(data.frame_id),
-                    else => {},
+                    .extended_header => |*extended_header| extended_header.deinit(),
+                    .header => {},
                 }
             }
         };
@@ -209,20 +417,23 @@ pub fn Parser(comptime ReaderType: type) type {
                             };
                         }
                     },
+                    // 0b00100000
+                    // 0b0bcd0000
                     .reading_extended_header => |payload| {
                         const extended_header_size = try self.reader.readIntBig(u32);
-                        _ = try self.reader.skipBytes(extended_header_size - 4, .{});
-                        // const number_of_flag_bytes = try self.reader.readByte();
-                        // // TODO(haze): we could probably remove this
-                        // var flag_bytes = try self.allocator.alloc(u8, number_of_flag_bytes);
-                        // _ = try self.reader.readAll(flag_bytes);
-                        // const raw_extended_header = RawExtendedHeader{
-                        //     .allocator = self.allocator,
-                        //     .flags = flag_bytes,
-                        //     .size = extended_header_size,
-                        // };
+                        const number_of_flag_bytes = try self.reader.readByte();
+                        log.warn("size={}, num_flags={}", .{ extended_header_size, number_of_flag_bytes });
+                        // TODO(haze): we could probably remove this
+                        var flag_bytes = try self.allocator.alloc(u8, number_of_flag_bytes);
+                        _ = try self.reader.readAll(flag_bytes);
+                        var raw_extended_header = RawExtendedHeader{
+                            .allocator = self.allocator,
+                            .flags = flag_bytes,
+                            .size = extended_header_size,
+                        };
+                        defer raw_extended_header.deinit();
                         self.state = .{ .reading_frame_or_padding = .{ .bytes_left = payload.bytes_left - extended_header_size } };
-                        // return Result{ .extended_header = raw_extended_header };
+                        return Result{ .extended_header = try raw_extended_header.interpret(self.reader) };
                     },
                     .reading_header => {
                         const flags = try self.reader.readByte();
