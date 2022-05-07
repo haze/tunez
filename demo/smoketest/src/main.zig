82c998139ebc6448f99378a2e429adee63846402
Haze Booth <isnt@haze.cool>
2022-05-07 19:16:51 -0400

    update smoketest to support new flac parsing

--- demo/smoketest/src/main.zig
+++ demo/smoketest/src/main.zig
@@ -18,11 +18,14 @@ pub fn main() anyerror!void {
 
     var library_walker = try library_dir.walk(arena.allocator());
 
-    var successful_file_count: usize = 0;
+    var successful_flac_file_count: usize = 0;
+    var successful_mp3_file_count: usize = 0;
     var file_count: usize = 0;
 
-    var file_times = std.ArrayList(u64).init(arena.allocator());
-    defer file_times.deinit();
+    var mp3_file_times = std.ArrayList(u64).init(arena.allocator());
+    defer mp3_file_times.deinit();
+    var flac_file_times = std.ArrayList(u64).init(arena.allocator());
+    defer flac_file_times.deinit();
 
     var id3_v3_unknown_frames_set = std.StringHashMap(void).init(arena.allocator());
     defer id3_v3_unknown_frames_set.deinit();
@@ -31,7 +34,9 @@ pub fn main() anyerror!void {
 
     while (try library_walker.next()) |entry| {
         if (entry.kind != .File) continue;
-        if (!std.mem.endsWith(u8, entry.basename, "mp3")) continue;
+        const is_mp3 = std.mem.endsWith(u8, entry.basename, "mp3");
+        const is_flac = std.mem.endsWith(u8, entry.basename, "flac");
+        if (!is_mp3 and !is_flac) continue;
 
         out.info("Reading {s}", .{entry.basename});
         defer {
@@ -42,36 +47,59 @@ pub fn main() anyerror!void {
         defer file.close();
         var reader = std.io.bufferedReader(file.reader()).reader();
 
-        var parser = tunez.id3.Parser(@TypeOf(reader)){
-            .allocator = arena.allocator(),
-            .reader = reader,
-        };
         var timer = try std.time.Timer.start();
-        while (parser.nextItem() catch |err| {
-            out.err("error during {s}: {}", .{ entry.basename, err });
-            continue;
-        }) |*result| {
-            defer result.deinit();
+        if (is_mp3) {
+            var parser = tunez.id3.Parser(@TypeOf(reader)){
+                .allocator = arena.allocator(),
+                .reader = reader,
+            };
+            while (parser.nextItem() catch |err| {
+                out.err("error during {s}: {}", .{ entry.basename, err });
+                continue;
+            }) |*result| {
+                defer result.deinit();
 
-            switch (result.*) {
-                .v3 => |*v3_result| switch (v3_result.*) {
-                    .unknown_frame => |*data| try id3_v3_unknown_frames_set.put(try arena.allocator().dupe(u8, data.frame_id), {}),
-                    else => {},
-                },
-                .v4 => |*v4_result| switch (v4_result.*) {
-                    .unknown_frame => |*data| try id3_v4_unknown_frames_set.put(try arena.allocator().dupe(u8, data.frame_id), {}),
-                    else => {},
-                },
+                switch (result.*) {
+                    .v3 => |*v3_result| switch (v3_result.*) {
+                        .unknown_frame => |*data| try id3_v3_unknown_frames_set.put(try arena.allocator().dupe(u8, data.frame_id), {}),
+                        else => {},
+                    },
+                    .v4 => |*v4_result| switch (v4_result.*) {
+                        .unknown_frame => |*data| try id3_v4_unknown_frames_set.put(try arena.allocator().dupe(u8, data.frame_id), {}),
+                        else => {},
+                    },
+                }
+                out.info("{}", .{result});
             }
-            out.info("{}", .{result});
-        }
+            try mp3_file_times.append(timer.read());
+            successful_mp3_file_count += 1;
+        } else if (is_flac) {
+            var parser = tunez.flac.Parser(@TypeOf(reader)){
+                .allocator = arena.allocator(),
+                .reader = reader,
+            };
+            while (parser.nextItem() catch |err| {
+                out.err("error during {s}: {}", .{ entry.basename, err });
+                continue;
+            }) |*result| {
+                defer result.deinit(arena.allocator());
 
-        try file_times.append(timer.read());
-        successful_file_count += 1;
+                out.info("{}", .{result});
+            }
+            try flac_file_times.append(timer.read());
+            successful_flac_file_count += 1;
+        }
     }
-    var total_time: u64 = 0;
-    for (file_times.items) |time|
-        total_time += time;
+
+    var flac_total_time: u64 = 0;
+    for (flac_file_times.items) |time|
+        flac_total_time += time;
+
+    var mp3_total_time: u64 = 0;
+    for (mp3_file_times.items) |time|
+        mp3_total_time += time;
+
+    const total_time = mp3_total_time + flac_total_time;
 
     const stdout = std.io.getStdOut().writer();
     try stdout.print("id3v2.3 unknown frames: {}\n", .{id3_v3_unknown_frames_set.count()});
@@ -92,5 +120,19 @@ pub fn main() anyerror!void {
         }
         try stdout.writeByte('\n');
     }
-    try stdout.print("parsed {}/{} files in {}\n", .{ successful_file_count, file_count, std.fmt.fmtDuration(total_time) });
+
+    var mp3_avg_time = @intToFloat(f64, mp3_total_time) / @intToFloat(f64, successful_mp3_file_count);
+    var flac_avg_time = @intToFloat(f64, flac_total_time) / @intToFloat(f64, successful_flac_file_count);
+
+    const successful_file_count = successful_mp3_file_count + successful_flac_file_count;
+
+    try stdout.print("parsed {} mp3, {} flac ({} total)/{} files in {} ({} avg for mp3, {} avg for flac)\n", .{
+        successful_mp3_file_count,
+        successful_flac_file_count,
+        successful_file_count,
+        file_count,
+        std.fmt.fmtDuration(total_time),
+        std.fmt.fmtDuration(@floatToInt(u64, @round(mp3_avg_time))),
+        std.fmt.fmtDuration(@floatToInt(u64, @round(flac_avg_time))),
+    });
 }
